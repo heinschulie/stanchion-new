ddf78d1d3ab211bf4a98c26502e7d19e5d6c4d01
Hein <heinschulie@gmail.com>
2015-08-27 11:48:41 +0200

    Update_NavLinkOrder

--- wp-content/themes/stanchion/header.php
+++ wp-content/themes/stanchion/header.php
@@ -138,13 +138,13 @@ endif; ?>
 			<nav class="navbar-collapse bs-navbar-collapse collapse" role="navigation"   id="site-navigation">
 
 				<ul class="nav navbar-nav navbar-right responsive-nav main-nav-list initial-navlist" >
-					<li class="page_item page-item-12"><a href="http://batstone-stanchiondev.azurewebsites.net/careers/">Careers</a></li>
-					<li class="page_item page-item-12"><a href="http://batstone-stanchiondev.azurewebsites.net/team/">Team</a></li>
 					<li class="page_item page-item-12"><a href="http://batstone-stanchiondev.azurewebsites.net/#whatwedo">What We Do</a></li>
 					<li class="page_item page-item-12"><a href="http://batstone-stanchiondev.azurewebsites.net/#results">Results</a></li>
 					<li class="page_item page-item-12"><a href="http://batstone-stanchiondev.azurewebsites.net/#clients">Clients</a></li>
 					<li class="page_item page-item-12"><a href="http://batstone-stanchiondev.azurewebsites.net/#howwethink">How We Think</a></li>
 					<li class="page_item page-item-12"><a href="http://batstone-stanchiondev.azurewebsites.net/#aboutus">About Us</a></li>	
+					<li class="page_item page-item-12"><a href="http://batstone-stanchiondev.azurewebsites.net/careers/">Careers</a></li>
+					<li class="page_item page-item-12"><a href="http://batstone-stanchiondev.azurewebsites.net/team/">Team</a></li>
 					<li class="page_item page-item-12"><a href="http://batstone-stanchiondev.azurewebsites.net/#contact">Contact</a></li>	
 					<li><a target="_blank" href="'.esc_url(__($zerif_socials_twitter,'zerif-lite')).'"><i class="fa fa-twitter"></i></a></li>	
 					<li><a target="_blank" href="'.esc_url(__($zerif_socials_linkedin,'zerif-lite')).'"><i class="fa fa-linkedin"></i></a></li>				

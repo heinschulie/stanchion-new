9205677eb5235a1afff52db4065784f3eb0d559c
Hein <heinschulie@gmail.com>
2015-09-01 08:56:36 +0200

    Experiment_Plugin

--- wp-content/themes/stanchion/template-stanchionjobs.php
+++ wp-content/themes/stanchion/template-stanchionjobs.php
@@ -20,13 +20,30 @@ get_header(); ?>
 
 		<main id="main" class="site-main" role="main">
 
+      
+      
 			<?php while ( have_posts() ) : the_post(); ?>
 
 			  <?php get_template_part( 'content', 'page-jobs' ); ?>
 
 			<?php endwhile; // end of the loop. ?>
+      
+      <div class="col-lg-12 col-md-12 col-sm-12" style="border: 1px solid red;">
+        <ul class="">
+          <?php // Display blog posts on any page @ http://m0n.co/l
+		      $args = array(
+	          'region' => 'Cape Town'
+          );
+          $wp_query = new WP_Query( $args );
+		      while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
+            <?php get_template_part( 'content', 'page-jobs' ); ?>
+          <?php endwhile; ?>
+        </ul>
+      </div>
 
-		</main><!-- #main -->
+
+
+    </main><!-- #main -->
 
 	</div><!-- #primary -->
 

d306d89468eccfdf914cdbf2f9b85b21205c5bb4
Hein <heinschulie@gmail.com>
2015-09-01 11:34:17 +0200

    Experiment_JBL_2

--- wp-content/themes/stanchion/content-page-jobs.php
+++ wp-content/themes/stanchion/content-page-jobs.php
@@ -41,7 +41,8 @@
 					echo '</div>';
 
 					echo '<div class="row" data-scrollreveal="enter left after 0s over 0.8s">';
-						theme_dynamic_sidebar( 'sidebar-ourjobs', 'Cape Town' );
+						//theme_dynamic_sidebar( 'sidebar-ourjobs', 'Cape Town' );
+            <h1>HELLO!</h1>
 					echo '</div> ';
 
 				echo '</div>';

52e6ea5d9d211b8a7c972a7dfc4a59c888170d16
Hein <heinschulie@gmail.com>
2015-09-07 16:19:43 +0200

    Cat_Fixes_2

--- wp-content/themes/stanchion/sections/how_we_think.php
+++ wp-content/themes/stanchion/sections/how_we_think.php
@@ -45,7 +45,7 @@
     <ul class="">
       <?php // Display blog posts on any page @ http://m0n.co/l
 		    $args = array(
-	        'category_name' => 'White Paper', 'Case Study'
+	        'category_name' => 'Case Study', 'White Paper'; 
         );
         $wp_query = new WP_Query($args);
 		    while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
